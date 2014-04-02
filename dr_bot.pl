1f1a6635e0994a298521b48bd51d1c123ed2b3c2
Joel Mertanen <joel.mertanen@gmail.com>
2014-04-02 19:02:53 +0300

    cleaned code, added support for wikipedia

--- dr_bot.pl
+++ dr_bot.pl
@@ -1,6 +1,9 @@
+use WWW::Wikipedia;
+use utf8;
+
 #http://stackoverflow.com/questions/7863746/perl-irssi-scripting-how-to-send-msg-to-a-specific-channel
 #name of the channels where this feature will be used
-my @channels  = ("#terassillekohta", "#testataanbottiakohta", "!inkubio");
+my %channels = { "#terassillekohta" => 1, "#testataanbottiakohta" => 1, "!inkubio" => 1);
 
 my @sentences = (
     "tuntuu hullulta",
@@ -24,53 +27,62 @@ my @sentences = (
 my $frequency = 50; #say something with 1/frequency probability
 
 #the public commands
-#own nick
-my $ownnick = "dr_bot";
 
 sub bootstrap {
     my ($server, $msg, $nick, $address, $target) = @_;
     #lowercase of the channel name in case this one will be registered in camelCase ;)
     $target = lc $target;
 
+    my $ownNick = $server->{nick};
+
     #wait a bit to appear more natural
     sleep(int(rand(5)) + 1);
 
-    foreach my $channel (@channels) {
-        if ( $target eq $channel) {
-            if (index($msg, $ownnick) != -1) {
-		@words = split(' ', $msg);
-                $whatword = @words[int(rand(@words))];
-                if( index( $whatword, $ownnick ) != -1 ) {
-                    $server->command("MSG $target $nick: sitä just.");
-		}
-                else {
-                     $kontti = "";
-                     for($i = 1; $i < @words; $i++) {
-                         $word = @words[$i];
-                         $word =~ s/\p{Punct}//g;
-                         $word =~ m/[cdfghjklmnpqrstvwxyz]/;
-                         $index = $-[0];
-                         if($index == 0) {
-                             $tmp = substr($word, 1);
-                             $tmp =~ m/[cdfghjklmnpqrstvwxyz]/;
-                             $index = $-[0] + 1;
-                             if($-[0] == 0) {
-                                  $tmp = substr($tmp, 1);
-                                  $tmp =~ m/[cdfghjklmnpqrstvwxyz]/;
-                                  $index = $-[0]+2;
-                             }
-                             if($index == 0){
-                                  $index = length($word);
-                             }
+    # charset are the shit
+    if ($msg =~ /^mikÃ¤ on (\w+)?/i) {
+        my $wikipedia = WWW::Wikipedia->new(language => 'en');
+        my $entry = $wikipedia->search($1);
+        if ($entry) {
+            $server->command("MSG $target found: " . $entry->text());
+        }
+        return;
+    }
 
-                         }                          
+    # old code
+    if ($channels->{$target}) {
+        if (index($msg, $ownNick) != -1) {
+	        @words = split(' ', $msg);
+            $whatword = @words[int(rand(@words))];
+            if( index( $whatword, $ownNick ) != -1 ) {
+                $server->command("MSG $target $nick: sitä just.");
+	        } else {
+                 $kontti = "";
+                 for($i = 1; $i < @words; $i++) {
+                     $word = @words[$i];
+                     $word =~ s/\p{Punct}//g;
+                     $word =~ m/[cdfghjklmnpqrstvwxyz]/;
+                     $index = $-[0];
+                     if($index == 0) {
+                         $tmp = substr($word, 1);
+                         $tmp =~ m/[cdfghjklmnpqrstvwxyz]/;
+                         $index = $-[0] + 1;
+                         if($-[0] == 0) {
+                              $tmp = substr($tmp, 1);
+                              $tmp =~ m/[cdfghjklmnpqrstvwxyz]/;
+                              $index = $-[0]+2;
+                         }
+                         if($index == 0){
+                              $index = length($word);
+                         }
+
+                     }                          
 
 
-                         $beginning = substr($word, 0, $index);
-                         $ending = substr($word, $index);
-                         $kontti = $kontti . "ko" . $ending . " " . $beginning . "ntti ";
-                     }
-                     $server->command("MSG $target $nick: $kontti");
+                     $beginning = substr($word, 0, $index);
+                     $ending = substr($word, $index);
+                     $kontti = $kontti . "ko" . $ending . " " . $beginning . "ntti ";
+                 }
+                 $server->command("MSG $target $nick: $kontti");
 
 #                    $whatword =~ s/\p{Punct}//g;
 #                    if(index($msg, 'jo') != -1) {
@@ -82,15 +94,13 @@ sub bootstrap {
 #                    else {
 #                        $server->command("MSG $target $nick: mitä " . $whatword . "kin nyt on");
 #                    }
-                }
             }
-            else {
-                $whattodo = int(rand( $frequency * @sentences ));
-                if( $whattodo < @sentences ) {
-                    $server->command("MSG $target @sentences[$whattodo]");
-                }
+        } else {
+            $whattodo = int(rand( $frequency * @sentences ));
+            if( $whattodo < @sentences ) {
+                $server->command("MSG $target @sentences[$whattodo]");
             }
-         }
+        }
      }
 }
 
@@ -99,7 +109,7 @@ sub private_msg {
     if( $nick eq "om" ) {
         $server->command("MSG #testataanbottiakohta $msg");
     }
-    if( $nick eq "dr_dom" ) {
+    if( $nick eq "dr_dom" || $nick eq "hu1" ) {
         $server->command("MSG #terassillekohta $msg");
     }
 }
