100ca9bc8eefb54484b44ba2c68b70824f1eade4
vatte <vatte.wikstrom@gmail.com>
2015-02-10 01:12:31 +0000

    fixing wikipedia, disabling wikipedia -> needs html parsing

--- dr_bot.pl
+++ dr_bot.pl
@@ -3,7 +3,7 @@ use utf8;
 
 #http://stackoverflow.com/questions/7863746/perl-irssi-scripting-how-to-send-msg-to-a-specific-channel
 #name of the channels where this feature will be used
-my %channels = { "#terassillekohta" => 1, "#testataanbottiakohta" => 1, "!inkubio" => 1);
+my %channels = { "#terassillekohta" => 1, "#testataanbottiakohta" => 1, "!inkubio" => 1};
 
 my @sentences = (
     "tuntuu hullulta",
@@ -26,6 +26,9 @@ my @sentences = (
 
 my $frequency = 50; #say something with 1/frequency probability
 
+my $wikipedia = WWW::Wikipedia->new(language => 'en');
+
+
 #the public commands
 
 sub bootstrap {
@@ -36,20 +39,23 @@ sub bootstrap {
     my $ownNick = $server->{nick};
 
     #wait a bit to appear more natural
-    sleep(int(rand(5)) + 1);
+    #sleep(int(rand(5)) + 1);
 
     # charset are the shit
-    if ($msg =~ /^mikÃ¤ on (\w+)?/i) {
-        my $wikipedia = WWW::Wikipedia->new(language => 'en');
-        my $entry = $wikipedia->search($1);
-        if ($entry) {
-            $server->command("MSG $target found: " . $entry->text());
-        }
-        return;
-    }
+#    if ($msg =~ /^mikÃ¤ on (\w+)?/i) {
+#        my $wikipedia = WWW::Wikipedia->new(language => 'en');
+#        my $entry = $wikipedia->search($1);
+#        if ($entry) {
+#            $server->command("MSG $target found: " . $entry->text());
+#        }
+#        return;
+#    }
 
     # old code
-    if ($channels->{$target}) {
+#    if (%channels->{$target}) {
+        #wait a bit to appear more natural
+        sleep(int(rand(5)) + 1);
+
         if (index($msg, $ownNick) != -1) {
 	        @words = split(' ', $msg);
             $whatword = @words[int(rand(@words))];
@@ -101,7 +107,7 @@ sub bootstrap {
                 $server->command("MSG $target @sentences[$whattodo]");
             }
         }
-     }
+#     }
 }
 
 sub private_msg {
